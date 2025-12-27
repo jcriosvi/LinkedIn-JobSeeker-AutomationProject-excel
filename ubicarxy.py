a73fcfccf0b2b238a888fec4150e3e2f773d8bfe
Juan Rios <jcriosvi@yahoo.com>
2025-12-27 13:35:49 -0500

    To solve the trace trouble in Automation code

--- ubicarxy.py
+++ ubicarxy.py
@@ -7,3 +7,4 @@ currentMouseX, currentMouseY = pyautogui.position()
 print(currentMouseX, currentMouseY)
 
 #https://www.linkedin.com/jobs/search-results/?f_EA=true&f_TPR=r86400&keywords=IT%20Project%20Manager&sortBy=R
+
